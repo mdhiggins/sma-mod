85f70d5335fe9421e38331dd44989763b7dd91e1
Michael Higgins <mdhiggins23@icloud.com>
2021-04-11 02:46:23 -0400

    environment fix

--- extras/postSonarr.sh
+++ extras/postSonarr.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/with-contenv bash
 
 $SMA_PATH/venv/bin/python3 $SMA_PATH/postSonarr.py
--- root/etc/profile.d/sma_path.sh
+++
@@ -1,4 +0,0 @@
-# env check
-if [[ -z "${SMA_PATH}" ]]; then
-    export SMA_PATH="/usr/local/sma"
-fi
